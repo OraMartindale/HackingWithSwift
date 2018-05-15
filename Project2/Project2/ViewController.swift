a647f67b801b82016c03636c8b42d8174de8b29e
Ora Martindale <ora@martindale.org>
2018-05-15 15:58:02 -0500

    Committing wip.

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    let countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for button in [button1, button2, button3] {
+            button!.layer.borderWidth = 1
+            button!.layer.borderColor = UIColor.lightGray.cgColor
+        }
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        for (i, button) in [button1, button2, button3].enumerated() {
+            button!.setImage(UIImage(named: countries[i]), for: .normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
