36e11285dcc6554b5f03d97c1701af2e73a6143f
Ora Martindale <ora@martindale.org>
2018-05-15 17:57:52 -0500

    Finished Project 2.

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Ora Martindale. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,7 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
-    let countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    var countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    var correctAnswer = 0
     var score = 0
     
     override func viewDidLoad() {
@@ -28,10 +30,27 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
         for (i, button) in [button1, button2, button3].enumerated() {
             button!.setImage(UIImage(named: countries[i]), for: .normal)
         }
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        title = countries[correctAnswer].uppercased()
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
