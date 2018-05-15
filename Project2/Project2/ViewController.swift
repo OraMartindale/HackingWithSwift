adbe7c7906a8039674db78b1a6167890a9b9a24e
Ora Martindale <ora@martindale.org>
2018-05-15 15:23:46 -0500

    Initial check in of Project 2.

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Ora Martindale on 5/15/18.
+//  Copyright © 2018 Ora Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
