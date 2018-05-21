7bd860b48328ccb979918107e60259671c54e0f6
Ora Martindale <ora@martindale.org>
2018-05-20 19:09:02 -0500

    WIP on Project5

---
+++ Project5/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Ora Martindale on 5/20/18.
+//  Copyright © 2018 Ora Martindale. All rights reserved.
+//
+
+import GameplayKit
+import UIKit
+
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        startGame()
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] _ in
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+
+    func submit(answer: String) {
+        print(answer)
+    }
+
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+}
+
