c5e0ecceedc8a83b63e8848c60f2c754f84365e6
Ora Martindale <ora@martindale.org>
2018-07-19 21:56:02 -0500

    Finished Project 7

--- Project7/DetailViewController.swift
+++ Project7/DetailViewController.swift
@@ -13,5 +13,20 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["body"] {
+            var html = [String]()
+            html.append("<html>")
+            html.append("<head>")
+            html.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">")
+            html.append("<style> body { font-size: 150%; } </style>")
+            html.append("</head>")
+            html.append("<body>")
+            html.append(body)
+            html.append("</body>")
+            html.append("</html>")
+            webView.loadHTMLString(html.joined(), baseURL: nil)
+        }
     }
 }
--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 1 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100&signatureCountFloor=10000"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? String(contentsOf: url) {
@@ -22,9 +27,12 @@ class ViewController: UITableViewController {
 
                 if json["metadata"]["responseInfo"]["status"].intValue == 200 {
                     parse(json: json)
+                    return
                 }
             }
         }
+        
+        showError()
     }
 
     func parse(json: JSON) {
@@ -37,6 +45,18 @@ class ViewController: UITableViewController {
         }
         
         tableView.reloadData()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
