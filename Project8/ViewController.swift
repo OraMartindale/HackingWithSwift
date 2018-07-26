39ca4e3e2cc5b0f14f81cc322c3db4f6eb8f2b43
Ora Martindale <ora@martindale.org>
2018-07-25 19:43:15 -0500

    * Started Project 8 * Created Interface for 7 Little Words-like game

---
+++ Project8/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Ora Martindale on 7/20/18.
+//  Copyright © 2018 Ora Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func submitTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
