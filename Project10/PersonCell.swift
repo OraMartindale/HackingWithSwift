f4703d92a163cf001b7a418b2247333ec13fa326
Ora Martindale <ora@martindale.org>
2019-01-22 23:30:37 -0600

    Initial check in of Project 10

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Ora Martindale on 1/20/19.
+//  Copyright © 2019 Ora Martindale. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Project10/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Ora Martindale on 1/20/19.
+//  Copyright © 2019 Ora Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+        return cell
+    }
+}
+
