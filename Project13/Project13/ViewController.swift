6c9e504a802fd6eadb7659302bb8896a04a549d4
Ora Martindale <ora@martindale.org>
2019-03-08 20:30:07 -0600

    Initial check in of Project13

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Ora Martindale on 3/3/19.
+//  Copyright © 2019 Ora Martindale. All rights reserved.
+//
+
+import CoreImage
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+
+    var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+    }
+
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        currentImage = image
+
+        dismiss(animated: true)
+    }
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
