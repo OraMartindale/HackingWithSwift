67b3427867ef6595058dea14cc74df709ae9e1e4
Ora Martindale <ora@martindale.org>
2019-04-23 20:50:15 -0500

    Checking in Project 14, Step 1 (Setting Up)

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Ora Martindale on 4/23/19.
+//  Copyright © 2019 Ora Martindale. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
